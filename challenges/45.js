22d47c5c20cc9b1798013e7ba1674b2ab9975435
Taela <taela.luccia@gmail.com>
2021-02-09 10:31:51 -0800

    Remove comparative statement for int_array[1]

--- challenges/45.js
+++ challenges/45.js
@@ -10,13 +10,9 @@
 
 
 function max_end3(int_array){
-  if(int_array[0] >= int_array[1] && int_array[0] >= int_array[2]){
+  if(int_array[0] >= int_array[2]){
     int_array[1] = int_array[0];
     int_array[2] = int_array[0];
-    return int_array;
-  } else if(int_array[1] >= int_array[0] && int_array[1] >= int_array[2]){
-    int_array[0] = int_array[1];
-    int_array[2] = int_array[1];
     return int_array;
   } else{
     int_array[0] = int_array[2];
@@ -27,7 +23,7 @@ function max_end3(int_array){
   console.log(max_end3([11, 3, 11])); //expected => [11, 11, 11] actual => [11, 11, 11]
   console.log(max_end3([5 , 5, 1])); //expected => [5, 5, 5] actual => [5, 5, 5]
   console.log(max_end3([4, 1, 4])); //expected => [4, 4, 4] actual => [4, 4, 4]
-  console.log(max_end3([12, 45, 1])); //expected => [45, 45, 45] actual => [45, 45, 45]
-  console.log(max_end3([1, 1, 1])); //expected => [1, 1, 1] actual => [1, 1, 1]
+  console.log(max_end3([12, 45, 1])); //expected => [12, 12, 12] actual => [12, 12, 12]
+  console.log(max_end3([1, 100, 8])); //expected => [8, 8, 8] actual => [8, 8, 8]
 
 module.exports = max_end3;
